7e68b6123ab2ad270e4d1010b46701eeb29cba31
Kunle Balogun <kunle@kunlebalogun.com>
2023-10-23 17:24:21 +0100

    Create V0005__Create_new_table.sql
    Replaced file with new file name

---
+++ sql/V0005__Create_new_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cust.customer6 (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(20),
+    address TEXT
+);
